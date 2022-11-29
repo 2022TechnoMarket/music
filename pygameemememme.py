8448ae07726ea2fd3c7412fee4f4df2ac085d201
songyoutaeck <119112927+songyoutaeck@users.noreply.github.com>
2022-11-29 20:59:10 +0900

    Update pygameemememme.py

--- pygameemememme.py
+++ pygameemememme.py
@@ -15,9 +15,15 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 #화면 타이틀 설정
 pygame.display.set_caption("HwanE Game")
 
+
+
+#<배경음악 사용방법>
 background = pygame.image.load("C:\\Users\\송유택\\OneDrive\\바탕 화면\\pymata4-master\\examples\\practice\\888.png")
+
 test_sound1 = pygame.mixer.Sound("C:\\Users\\송유택\\OneDrive\\바탕 화면\\pymata4-master\\examples\\practice\\minigameT11.mp3")
-test_sound1.play(-1)
+test_sound1.play(-1) #<무한히 반복>
+
+
 # test_sound2 = pygame.mixer.Sound("C:\\Users\\송유택\\OneDrive\\바탕 화면\\pymata4-master\\examples\\practice\\minigameT2.mp3")
 # test_sound2.play(-1)
 # test_sound3 = pygame.mixer.Sound("C:\\Users\\송유택\\OneDrive\\바탕 화면\\pymata4-master\\examples\\practice\\GameOver1.mp3")
@@ -27,13 +33,13 @@ test_sound1.play(-1)
 running = True  
 while running:
     
-    for event in pygame.event.get(): #이벤트의 발생 여부에 따른 반복문
-        if event.type == pygame.QUIT: #창을 닫는 이벤트 발생했는가?
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: 
             running = False
-    screen.blit(background, (0, 0)) #배경에 이미지 그려주고 위치 지정
+    screen.blit(background, (0, 0)) 
     pygame.display.update()
-    time.sleep(5)
-    test_sound1.stop() 
+    time.sleep(5) #5초만 동작후
+    test_sound1.stop()  #멈춤
 pygame.quit() 
 
 
